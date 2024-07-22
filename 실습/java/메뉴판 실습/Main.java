419d9f820b96127dfd4aaa0c0f0f051637b8ea96
kkw2238 <kkw2238@naver.com>
2024-07-22 21:11:13 +0900

    2024.07.22 실습 및 Java 강의 내용 추가

---
+++ 실습/java/메뉴판 실습/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) 
+    {
+        String menu;
+        String[] recipe;
+        double score;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("메뉴 : ");
+        
+    }
+}
